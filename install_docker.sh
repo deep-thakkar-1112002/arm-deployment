b56551fdf8c5eb5541101f4264b6c59be52dcc4d
deep-thakkar-1112002 <129286565+deep-thakkar-1112002@users.noreply.github.com>
2024-02-06 17:47:41 +0530

    Add files via upload

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,7 @@
+echo "Hostname=$1" > "netskope_config.env"
+echo "Token=$2" >> "netskope_config.env"
+echo "WorkspaceKey=$3" >> "sentinel_config.env"
+echo "WorkspaceId=$4" >> "sentinel_config.env"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo docker pull deep1112002/netskopewebtransactions:nskpwebtransactions
