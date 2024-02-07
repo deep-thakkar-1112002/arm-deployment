cd6ca523b453e1273998b22a0f1779d77dc8473d
Deep Thakkar <deep.thakkar@crestdatasys.com>
2024-02-07 16:14:42 +0530

    Updated the shell script.

--- install_docker.sh
+++ install_docker.sh
@@ -1,9 +1,11 @@
+DOCKER_LINK = $1
 DATA_DIR="/home/devuser/docker_persistent_volume"
 mkdir /home/devuser/docker_persistent_volume
-echo "Hostname=$1" > "$DATA_DIR/netskope_config.env"
-echo "Token=$2" >> "$DATA_DIR/netskope_config.env"
-echo "WorkspaceKey=$3" >> "$DATA_DIR/sentinel_config.env"
-echo "WorkspaceId=$4" >> "$DATA_DIR/sentinel_config.env"
+echo "Hostname=$2" > "$DATA_DIR/netskope_config.env"
+echo "Token=$3" >> "$DATA_DIR/netskope_config.env"
+echo "WorkspaceKey=$4" >> "$DATA_DIR/sentinel_config.env"
+echo "WorkspaceId=$5" >> "$DATA_DIR/sentinel_config.env"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-sudo docker pull deep1112002/netskopewebtransactions:nskpwebtransactions
+sudo docker pull $DOCKER_LINK
+sudo docker run -d -v $(pwd)/docker_persistent_volume:/app $DOCKER_LINK
