c0a1ae53e394dcf0d115b9a9ae5640cf9f177fa7
Deep Thakkar <deep.thakkar@crestdatasys.com>
2024-02-08 18:27:42 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -9,7 +9,7 @@ echo "WorkspaceKey=$4" >> "$DATA_DIR/sentinel_config.env"
 echo "WorkspaceId=$5" >> "$DATA_DIR/sentinel_config.env"
 echo "BackoffRetryCount=$6" > "$DATA_DIR/general_config.env"
 echo "BackoffSleepTime=$7" >> "$DATA_DIR/general_config.env"
-echo "*IdleTimeout=$8" >> "$DATA_DIR/general_config.env"
+echo "IdleTimeout=$8" >> "$DATA_DIR/general_config.env"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo docker pull $DOCKER_LINK
